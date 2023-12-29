45e756c300bdbce37882e205049651a292b366c7
HiImWhite <wojttasek1999@gmail.com>
2023-12-29 14:25:49 +0100

    first commit

---
+++ dadJokes.py
@@ -0,0 +1,25 @@
+dadJokes = [
+    ['Co robi dziadek na dyskotece? - Tańczy z laską.'],
+    ['Dlaczego długopisy nie chodzą do szkoły? - Bo się wypisały.'],
+    ['Jasiu idzie przez las i znika.'],
+    ['Czemu woda niezdała egzaminu? Bo oblała.'],
+    ['Uczestnicy w kole fortuny się przedstawiają: K jak Krzysztof\nproszę pana najpierw się przedstawiamy a potem mówimy litery\nK jak Krzysztof\nproszę pana najperw sie przedstawiamy!\ndobrze, Krzysztof Kajak.'],
+    ['Niewidomy mówi do niewidomego: Chcesz się przejść?\nDrugi odpowiada: Nie widzę przeszkód.'],
+    ['Wiesław zobaczył na plaży dla nudystów kobietę swoich marzeń. I wszyscy na plaży nudystów zobaczyli, że Wiesław ją zobaczył.'],
+    ['Przychodzi baba do lekarza cała w muchach i lekarz się pyta: \n- Co panią do mnie sprowadza? \n- Zostałam zmuszona.'],
+    ['Moja córka przyłapała mnie na seksie z prostytutką i się wściekła. Już nie są najlepszymi przyjaciółkami.'],
+    ['Nie chcę powiedzieć, że moja była była otyła, ale mój materac z efektem pamięci potrzebował roku, żeby o niej zapomnieć.'],
+    ['- Każdemu mężczyźnie dane są dwa metry. Jak je rozlokuje, to jego sprawa \n- rzekł mój dziadek (175 cm wzrostu) do nowego chłopaka mojej siostry (198 cm wzrostu).'],
+    ['Marian jest tak skrytym człowiekiem, że o tym, że miał orgazm, kobiety dowiadują się dopiero z dwóch pasków na teście.'],
+    ['W tym roku w święta nie będziemy grali w Kalambury, po tym jak w zeszłym roku babcia pokazała "Nagi Instynkt".'],
+    ['Demokracja jest jak gwałt zbiorowy. Mniejszość może być przeciwna, ale kogo to obchodzi?'],
+    ['Czym się różnią Chińczycy od rasizmu? - Rasizm ma wiele twarzy.'],
+    ['Podczas pandemii gadają dwaj Finowie: \n- Słyszałeś że z powodu wirusa są obowiązkowe 2-metrowe odstępy? \n- Co jest? Czemu tak blisko?'],
+    ['Najnowszy hit książkowy w Walii: "Sto sposobów na owce". Jest w niej nawet kilka przepisów kulinarnych.'],
+    ['Starszy facet pyta młodej dziewczyny: \n- Jak pani sądzi, ile mam lat? \n- Hm... dała bym panu... \n- Och, dziękuję bardzo!'],
+    ['Mąż kładzie się do łóżka i szepcze do żony: \n- Nie mam slipek. \n- Jutro Ci upiorę.'],
+    ['Krzysiek pisze SMSa do Mamy: \n- Cześć Mamusiu, o której będziesz? Bardzo się martwię :) \n- Nie zdążycie, będę za 5 minut, pozdrów Karolinę.'],
+    ['Mąż i żona oglądają w telewizji program psychologiczny o skrajnych emocjach. W pewnym momencie odzywa się mąż: \n- Kochanie, to wszystko stek bzdur. Założę się, że nie jesteś w stanie powiedzieć mi czegoś, co uczyni mnie radosnym i smutnym jednocześnie. \n- Masz największego penisa wśród swoich kumpli.']
+]
+
+formatted_jokes = ['```\n{}\n```'.format(joke[0]) for joke in dadJokes]
---
+++ discordLandBot.py
@@ -0,0 +1,90 @@
+import os
+import discord
+import requests
+import random
+
+from dotenv import load_dotenv
+from dadJokes import formatted_jokes
+from discord.ext import commands
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.all()
+intents.messages = True
+
+initial_activity = discord.Game(name="NFT Prices")
+
+bot = commands.Bot(command_prefix='/', intents=intents,
+                   activity=initial_activity)
+
+
+@bot.command()
+async def test(ctx, arg):
+    print("Command executed")
+    await ctx.send(arg)
+
+
+@bot.command()
+async def bothelp(ctx):
+    await ctx.send("Available commands: test (arg), joke, rename (arg), update.")
+
+
+@bot.command()
+async def joke(ctx):
+    random_index = random.randint(0, len(formatted_jokes) - 1)
+    random_joke = formatted_jokes[random_index]
+    await ctx.send(random_joke)
+
+
+@bot.command()
+async def rename(ctx, name):
+    await bot.user.edit(username=name)
+
+OPENSEA_API_URL = 'https://api.opensea.io/api/v2/listings/collection/pixels-farm/best?limit=1'
+API_KEY = os.getenv("OPENSEA_API_KEY")
+
+CRYPTOCOMPARE_API_URL = 'https://min-api.cryptocompare.com/data/price'
+CRYPTOCOMPARE_API_PARAMS = {'fsym': 'ETH', 'tsyms': 'USD,PLN'}
+
+
+@bot.command()
+async def update(ctx):
+    try:
+        headers = {'Accept': 'application/json', 'X-API-KEY': API_KEY}
+        response = requests.get(OPENSEA_API_URL, headers=headers)
+        response.raise_for_status()
+
+        collection_data = response.json()
+        listings = collection_data.get('listings', [])
+
+        if listings:
+            first_listing = listings[0]
+            price_in_wei = first_listing.get(
+                'price', {}).get('current', {}).get('value')
+
+            if price_in_wei:
+                price_in_eth = float(price_in_wei) / (10 ** 18)
+
+                eth_prices = requests.get(
+                    CRYPTOCOMPARE_API_URL, params=CRYPTOCOMPARE_API_PARAMS).json()
+                eth_price_usd = eth_prices.get('USD') * price_in_eth
+                eth_price_pln = eth_prices.get('PLN') * price_in_eth
+                eth_ilosc_harnas = float(eth_price_pln) / (3.39)
+
+                await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'💰 {eth_price_usd:.2f} USD | {eth_price_pln:.2f} PLN'))
+                await ctx.send(f'Collection floor price (first listing) updated to {price_in_eth} ETH | ${eth_price_usd:.2f} USD | {eth_price_pln:.2f} PLN | Harnaś cans {eth_ilosc_harnas:.0f} ')
+            else:
+                await ctx.send('Unable to fetch price from the first listing.')
+        else:
+            await ctx.send('No listings found in the response.')
+    except requests.exceptions.HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+        await ctx.send('An HTTP error occurred while fetching the collection floor price.')
+    except Exception as e:
+        print(f'An error occurred: {e}')
+        await ctx.send('An error occurred while fetching the collection floor price.')
+
+# EXECUTES THE BOT WITH THE SPECIFIED TOKEN.
+bot.run(DISCORD_TOKEN)
